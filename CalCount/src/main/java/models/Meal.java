b557605f32826b9c57c07a2b0f6d1c10612b39fe
jonathan.powers <jonathan.powers@revature.net>
2021-10-08 14:03:17 -0400

    Created Models

--- CalCount/src/main/java/models/Meal.java
+++ CalCount/src/main/java/models/Meal.java
@@ -1,5 +1,21 @@
 package models;
 
+import java.time.LocalDateTime;
+
 public class Meal {
+	int Id;
+	
+	String name;
+	
+	String description;
+	
+	/**
+	 * True if calories is negative
+	 */
+	boolean exercise;
+	
+	int calories;
+	
+	LocalDateTime date;
 
 }
--- CalCount/src/main/java/models/User.java
+++ CalCount/src/main/java/models/User.java
@@ -1,5 +1,25 @@
 package models;
 
-public class User {
+import java.util.List;
 
+public class User {
+	int Id;
+	
+	String name;
+	
+	String email;
+	
+	String password;
+	
+	/**
+	 * height in centimeters
+	 */
+	int height;
+	
+	/**
+	 * weight in kilograms
+	 */
+	int weight;
+	
+	List<Integer> friends;
 }
